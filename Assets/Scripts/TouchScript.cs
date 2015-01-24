eb0645646b14b709beb39424b37c61ea4844e112
lisa.lau07@gmail.com <lisa.lau07@gmail.com>
2015-01-23 20:03:43 -0500

    initial commit

---
+++ Assets/Scripts/TouchScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchScript : MonoBehaviour {
+	
+	void Update () {
+        
+    }
+
+    void OnGUI()
+    {
+        foreach (Touch t in Input.touches)
+        {
+            string message = "";
+            message += "ID: " + t.fingerId + "\n";
+
+        }
+    }
+
+}
